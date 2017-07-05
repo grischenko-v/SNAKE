d9855fffcc414663d8f09dddb42427b28ca393c3
Vladimir <boooobka@gmail.com>
2017-07-05 21:40:06 +0400

    up and down moving

--- main.js
+++ main.js
@@ -20,47 +20,75 @@ $(document).keypress(function(e) {
     	currentPositionX = rowSize ;
     	for(var i = 0; i < rowSize ; i++) nextPoint = nextPoint.next();
     } 
+    i=0;
     console.log(currentPositionX);
     tempPoint = nextPoint; 
     prevPoint = tempPoint;
     nextPoint.css("background-color", "#777");
-
     }//left
- /*  if(e.keyCode === 38 || e.which ===38){ 
-   console.log("up");
-   nextPoint = $("#first").next();
-   nextPoint.css("background-color", "#777");
-   }//up
-   */
-   if(e.keyCode === 39 || e.which ===39){
+    if(e.keyCode === 39 || e.which ===39){
    	console.log("right");
-
     prevPoint.css("background-color", "#ccc");
-     if(currentPositionX >= 0  &&  currentPositionX < rowSize){ currentPositionX++;  nextPoint = prevPoint.next();}
-
-    else{
+     if(currentPositionX >= 0  &&  currentPositionX < rowSize){
+        currentPositionX++;  
+        nextPoint = prevPoint.next();
+      }else{
     	currentPositionX = 0;
     	for(var i = 0; i < rowSize ; i++) nextPoint = nextPoint.prev();
-    } 
-
-  console.log(currentPositionX);
-       
-
-
-    tempPoint = nextPoint; 
-    prevPoint = tempPoint;
-    nextPoint.css("background-color", "#777");
+      } 
+      i=0;
+      console.log(currentPositionX);
+      tempPoint = nextPoint; 
+      prevPoint = tempPoint;
+      nextPoint.css("background-color", "#777");
     }//right
- /*  if(e.keyCode === 40 || e.which ===40){
-    console.log("down");
-     nextPoint = $("#first").next();
-     nextPoint.css("background-color", "#777");
-    }//down*/
+
+
+
+   if(e.keyCode === 38 || e.which ===38){ 
+    console.log("up");
+    prevPoint.css("background-color", "#ccc");
+
+     if(currentPositionY >= 0  &&  currentPositionY < rowSize){
+        currentPositionY++;  
+        for(var i = 0; i <= rowSize ; i++){ 
+            if(nextPoint) nextPoint =  nextPoint.prev();
+            else nextPoint= prevPoint.prev();    
+        }
+      }else{   
+    	for(var j = 0; j <= (rowSize+1) * 8 - 1; j++) nextPoint = nextPoint.next();
+    	currentPositionY = 0;	      
+      } 
+      j=0; i=0;
+      console.log(currentPositionY);
+      tempPoint = nextPoint; 
+      prevPoint = tempPoint;
+      nextPoint.css("background-color", "#777");
+   }//up
+
+ 
+   if(e.keyCode === 40 || e.which ===40){
+     console.log("down");
+     prevPoint.css("background-color", "#ccc");
+     if(currentPositionY > 0  &&  currentPositionY <= rowSize){
+        currentPositionY--;  
+        for(var i = 0; i <= rowSize ; i++){ 
+            if(nextPoint) nextPoint =  nextPoint.next();
+            else nextPoint= prevPoint.next();    
+        }
+      }else{   
+    	for(var j = 0; j <= (rowSize+1) *8 - 1; j++) nextPoint = nextPoint.prev();
+    	currentPositionY = rowSize;	      
+      } 
+      j=0; i=0;
+      console.log(currentPositionY);
+      tempPoint = nextPoint; 
+      prevPoint = tempPoint;
+      nextPoint.css("background-color", "#777");
+    }//down
 
 
 });
 }
 )
-//nextPoint.css("background-color", "#777");
-//$("#first").css("background-color", "#ddd");
 
